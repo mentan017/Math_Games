6bde70556286d46381a10c4f45b5d91b249e7c86
Adrian Bernabeu <mentan017@gmail.com>
2024-10-03 21:51:26 +0200

    Multiplication game is working now, missing difficulties

--- client/multiplication/script.js
+++ client/multiplication/script.js
@@ -1,6 +1,47 @@
-window.onload = GetMultiplication(0);
+var timerInterval;
+var solved = 0;
 
-document.getElementById("user-solution").addEventListener("keyup", CheckSolution);
+document.getElementById("start-btn").addEventListener("click", StartGame);
+
+function DisplayStart(){
+    document.getElementById("question-super-container").innerHTML = `
+    <div id="start-btn">Start <span style="font-size: 1.6rem;">&#9654;</span></div>`;
+    document.getElementById("start-btn").addEventListener("click", StartGame);
+}
+
+function StartGame(){
+    console.log("test");
+    if(timerInterval){
+        clearInterval(timerInterval);
+        solved = 0;
+        document.getElementById("timer").innerText= "00:00.00";
+    }
+    StartCountdown(3);
+}
+
+function StartCountdown(num){
+    if(num > 0){
+        document.getElementById("question-super-container").innerHTML = `<div class="countdown">${num}</div>`;
+        setTimeout(StartCountdown, 1000, num-1)
+    }else if(num == 0){
+        document.getElementById("question-super-container").innerHTML = `<div class="countdown">GO!</div>`;
+        setTimeout(StartCountdown, 400, num-1)
+    }else{
+        document.getElementById("question-super-container").innerHTML = `
+        <div id="question-container">
+            <div>x</div>
+            <div>
+            </div>
+        </div>
+        <div id="solution-container">
+            <input type="text" placeholder="000000" id="user-solution">
+        </div>`;
+        document.getElementById("question-super-container").classList.add("playing");
+        GetMultiplication(0);
+        timerInterval = setInterval(UpdateTimer, 67, (new Date).getTime());
+        document.getElementById("user-solution").addEventListener("keyup", CheckSolution);
+    }
+}
 
 function GetMultiplication(mode){
     var min = 10;
@@ -16,11 +57,6 @@ function GetMultiplication(mode){
 }
 
 function CheckSolution(){
-    //! Temporary
-    if(document.getElementById("timer").innerText == "00:00.00"){
-        document.getElementById("timer").innerText == "00:00:01";
-        setInterval(UpdateTimer, 67, (new Date).getTime());
-    }
     var input = parseInt(document.getElementById("user-solution").value);
     var operation = document.getElementById("question-container").getAttribute("data-operation");
     var factors = operation.split(".");
@@ -29,8 +65,20 @@ function CheckSolution(){
         product *= parseInt(factors[i]);
     }
     if(product === input){
-        GetMultiplication(0);
-        document.getElementById("user-solution").value = "";
+        solved++;
+        if(solved == 5){
+            clearInterval(timerInterval);
+            document.getElementById("question-super-container").classList.remove("playing");
+            document.getElementById("question-super-container").innerHTML = `
+            <div class="finish-screen">
+                <p>Congrats! You time is<br><span style="color: #aca916; line-height: 4rem;">${document.getElementById("timer").innerText}</span></p>
+                <p id="play-again-btn">Play again &#8634;</p>
+            </div>`;
+            document.getElementById("play-again-btn").addEventListener("click", DisplayStart);
+        }else{
+            GetMultiplication(0);
+            document.getElementById("user-solution").value = "";    
+        }
     }
 }
 
